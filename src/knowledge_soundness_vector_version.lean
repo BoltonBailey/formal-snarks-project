8ab5a2e03e71b359919320e42684ab8391912dce
Bolton Bailey <bolton.bailey@gmail.com>
2020-12-12 18:06:51 -0800

    refactor to use maps from fin rather than vectors

--- src/knowledge_soundness_vector_version.lean
+++ src/knowledge_soundness_vector_version.lean
@@ -39,11 +39,10 @@ def Y_poly : mv_polynomial vars F := mv_polynomial.X vars.Y
 def Z_poly : mv_polynomial vars F := mv_polynomial.X vars.Z
 
 
--- /-- Vector form of range function -/
--- def vector_range (k : ℕ) : vector ℕ k :=
--- ⟨list.range k, by simp⟩
--- /- TODO ask mathlib maintainers to add vector.range to mathlib -/
--- -- mathlib people suggest using fin n \to R instead of vectors
+/-- Vector form of range function -/
+def vector_range (k : ℕ) : vector ℕ k :=
+⟨list.range k, by simp⟩
+/- TODO ask mathlib maintainers to add vector.range to mathlib -/
 
 
 /-- The naturals representing the number of gates in the circuit, the statement size, and witness size repectively-/ 
@@ -52,83 +51,79 @@ def n := n_stmt + n_wit
 -- NOTE: In the paper, n_stmt is l and n_wit is n-l. Here, n is defined from these values.
 
 
-/-- u_stmt and u_wit are fin-indexed collections of polynomials from the square span program -/
-parameter {u_stmt : fin n_stmt → (polynomial F) }
-parameter {u_wit : fin n_wit → (polynomial F) }
-
+/-- u_stmt and u_wit are vectors of polynomials from the square span program -/
+parameter {u_stmt : vector (polynomial F) n_stmt}
+parameter {u_wit : vector (polynomial F) n_wit}
+/-- and u is their concatenation -/
+def u : (vector (polynomial F) n) := vector.append u_stmt u_wit
+-- NOTE: u is usually a universe variable in lean, 
+-- here it is a vector of polynomials to match the paper
 
 /-- The polynomial divisibility by which is used to verify satisfaction of the SSP -/
-def r : fin m → F := (λ i, (i : F))
-/-- (X - r_1) ... (X - r_m) -/
-def t : polynomial F := finset.prod (finset.fin_range m) (λ i, polynomial.X - polynomial.C (i.1 : F))
+def r : vector F m := (vector_range m).map (λ i, (i : F))
+def t : polynomial F := (r.map (λ i : F, polynomial.X - polynomial.C i)).to_list.prod -- (X - r_1) ... (X - r_m)
 
--- /-- Checks whether a witness satisfies the SSP -/
--- def satisfying_wit (a_stmt : vector F n_stmt) (a_wit : vector F n_wit) := 
--- polynomial.mod_by_monic (((vector.map₂ has_scalar.smul (vector.append a_stmt a_wit) u).to_list.sum)^2) t = 1
 
 /-- Checks whether a witness satisfies the SSP -/
-def satisfying_wit (a_stmt : fin n_stmt → F ) (a_wit : fin n_wit → F) := 
-polynomial.mod_by_monic ((finset.sum (finset.fin_range n_stmt) (λ i, a_stmt i • u_stmt i))^2) t = 1
+def satisfying_wit (a_stmt : vector F n_stmt) (a_wit : vector F n_wit) := 
+polynomial.mod_by_monic (((vector.map₂ has_scalar.smul (vector.append a_stmt a_wit) u).to_list.sum)^2) t = 1
+
 
 
 /-- The crs elements as multivariate polynomials of the toxic waste samples -/
-def crs_powers_of_τ : fin m → (mv_polynomial vars F) := (λ i, X_poly^(i : ℕ))
+def crs_powers_of_τ : vector (mv_polynomial vars F) m := vector.map (λ i, X_poly^i) (vector_range m)
 def crs_γ : mv_polynomial vars F := Z_poly
 def crs_γβ : mv_polynomial vars F := Z_poly * Y_poly
-def crs_β_ssps : fin n_wit → (mv_polynomial vars F) := (λ i, (Y_poly) * (u_wit i).eval₂ mv_polynomial.C X_poly) 
+def crs_β_ssps : vector (mv_polynomial vars F) n_wit := vector.map (λ p : polynomial F, (Y_poly) * p.eval₂ mv_polynomial.C X_poly) u_wit
 
 /-- The statement polynomial that the verifier computes from the statement bits-/
-def V_stmt (a_stmt : fin n_stmt → F) : polynomial F 
-:= finset.sum (finset.fin_range n_stmt) (λ i, a_stmt i • u_stmt i)
+def V_stmt (a_stmt : vector F n_stmt) : polynomial F 
+:= (vector.map₂ has_scalar.smul a_stmt u_stmt).to_list.sum
 
 /-- The coefficients of the CRS elements in the algebraic adversary's representation -/
-parameters {b v h : fin m → F}
+parameters {b v h : vector F m}
 parameters {b_γ v_γ h_γ b_γβ v_γβ h_γβ : F}
-parameters {b' v' h' : fin n_wit → F}
+parameters {b' v' h' : vector F n_wit}
 
 /-- Polynomial forms of the adversary's proof representation -/
 def B_wit  : mv_polynomial vars F := 
-  finset.sum (finset.fin_range m) (λ i, (b i) • (crs_powers_of_τ i))
+  (vector.map₂ has_scalar.smul b crs_powers_of_τ).to_list.sum
   +
   b_γ • crs_γ
   +
   b_γβ • crs_γβ
   +
-  finset.sum (finset.fin_range n_wit) (λ i, (b' i) • (crs_β_ssps i))
-
+  (vector.map₂ has_scalar.smul b' crs_β_ssps).to_list.sum
 
 
 def V_wit : mv_polynomial vars F := 
-  finset.sum (finset.fin_range m) (λ i, (v i) • (crs_powers_of_τ i))
+  (vector.map₂ has_scalar.smul v crs_powers_of_τ).to_list.sum
   +
   v_γ • crs_γ
   +
   v_γβ • crs_γβ
   +
-  finset.sum (finset.fin_range n_wit) (λ i, (v' i) • (crs_β_ssps i))
+  (vector.map₂ has_scalar.smul v' crs_β_ssps).to_list.sum
 
 def H : mv_polynomial vars F := 
-  finset.sum (finset.fin_range m) (λ i, (h i) • (crs_powers_of_τ i))
+  (vector.map₂ has_scalar.smul h crs_powers_of_τ).to_list.sum
   +
   h_γ • crs_γ
   +
   h_γβ • crs_γβ
   +
-  finset.sum (finset.fin_range n_wit) (λ i, (h' i) • (crs_β_ssps i))
+  (vector.map₂ has_scalar.smul h' crs_β_ssps).to_list.sum
 
 
 /-- Show that if the adversary polynomials obey the equations, then the coefficients give a satisfying witness -/
-lemma case_1 (a_stmt : fin n_stmt → F ) : 
-  (B_wit = Y_poly * V_wit) 
-  -> (H * (t.eval₂ mv_polynomial.C X_poly) + mv_polynomial.C 1 = (V_wit + (V_stmt a_stmt).eval₂ mv_polynomial.C X_poly)^2) 
-  -> (satisfying_wit a_stmt b')
+lemma case_1 (a_stmt : vector F n_stmt) : (B_wit = Y_poly * V_wit) -> (H * (t.eval₂ mv_polynomial.C X_poly) + mv_polynomial.C 1 = (V_wit + (V_stmt a_stmt).eval₂ mv_polynomial.C X_poly)^2) -> (satisfying_wit a_stmt b')
 :=
 begin
   intros eqnI eqnII,
   have h1 : (∀ m : vars →₀ ℕ, m vars.Y = 0 -> B_wit.coeff m = 0),
   /- TODO use lemma from mv_divisability -/
 
-  have h2 : b = (λ i, 0),
+  have h2 : b = vector.repeat 0 m,
 
 end
 
