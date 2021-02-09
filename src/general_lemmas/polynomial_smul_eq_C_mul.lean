bba8e7f43ae679074624cd954d2cb8edc4cb5e3c
Bolton Bailey <bolton.bailey@gmail.com>
2021-02-08 20:05:53 -0800

    completed simple lemma for smul_eq_C_mul

---
+++ src/general_lemmas/polynomial_smul_eq_C_mul.lean
@@ -0,0 +1,23 @@
+
+import data.polynomial.basic
+import data.polynomial.monomial
+import algebra.polynomial.group_ring_action
+
+namespace polynomial
+universes u
+variables {R : Type u} {a : R} {m n : ℕ}
+
+section semiring
+variables [semiring R] {p q : polynomial R}
+
+/-- A scalar multiplication is equivalent to constant polynomial multiplication for polynomials -/
+lemma smul_eq_C_mul (a : R) : (polynomial.C a) * p = a • p :=
+begin
+  rw polynomial.ext_iff,
+  intro n,
+  rw polynomial.coeff_C_mul,
+  rw polynomial.coeff_smul,
+end
+-- TODO create mathlib PR
+
+end
