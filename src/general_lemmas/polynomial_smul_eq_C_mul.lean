c728506698fce219e9c99841602e338671b29630
Bolton Bailey <bolton.bailey@gmail.com>
2021-05-15 14:35:14 -0400

    added todo to remove mathlibed file

--- src/general_lemmas/polynomial_smul_eq_C_mul.lean
+++ src/general_lemmas/polynomial_smul_eq_C_mul.lean
@@ -12,7 +12,8 @@ variables [semiring R] {p q : polynomial R}
 
 /-- A scalar multiplication is equivalent to constant polynomial multiplication for polynomials -/
 lemma smul_eq_C_mul (a : R) : a • p = (polynomial.C a) * p := by simp [ext_iff]
--- TODO create mathlib PR
+-- [mathlib PR](https://github.com/leanprover-community/mathlib/pull/7240)
+-- TODO this has now been merged, so it should be possible to upgrade the mathlib for this project and remove this file at some point
 
 end semiring
 end polynomial
