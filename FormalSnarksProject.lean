ebb713b79bdc2d986b43a114afae9fccea0e6b1a
Bolton Bailey <bolton.bailey@gmail.com>
2024-05-19 00:08:24 -0500

    add imports

--- FormalSnarksProject.lean
+++ FormalSnarksProject.lean
@@ -1,1 +1,5 @@
 import FormalSnarksProject.ToMathlib.PolynomialQuotient
+import FormalSnarksProject.ToMathlib.OptionEquivRight
+import FormalSnarksProject.ToMathlib.ForTransformations
+import FormalSnarksProject.Transformations.Transformations
+import FormalSnarksProject.Models.AGMProofSystemInstantiation
